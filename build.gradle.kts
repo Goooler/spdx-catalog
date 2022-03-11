f044c8e63c2920b2ff7c913d994635a82a2e976d
Klaus Lehner <172195+klu2@users.noreply.github.com>
2022-03-11 22:40:19 +0100

    add necessary pom data

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     `maven-publish`
 }
 
+description = "The SPDX license catalog including a Kotlin Wrapper for access"
 group = "io.cloudflight.license.spdx"
 version = "3.16"
 
@@ -10,21 +11,70 @@ repositories {
     mavenCentral()
 }
 
+java {
+    withJavadocJar()
+    withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest {
+        val configuration = project.configurations.getByName(JavaPlugin.RUNTIME_CLASSPATH_CONFIGURATION_NAME)
+        val classpath = configuration.files.joinToString(" ") { it.name }
+        val createdBy = "${System.getProperty("java.version")} (${System.getProperty("java.vendor")})"
+
+        attributes(
+            "Class-Path" to classpath,
+            "Created-By" to createdBy,
+            "Implementation-Vendor" to "Cloudflight",
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to project.version
+        )
+    }
 }
 
 publishing {
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
+            pom {
+                name.set(project.name)
+                description.set(project.description)
+                url.set("https://github.com/cloudflightio/spdx-catalog")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                inceptionYear.set("2022")
+                organization {
+                    name.set("Cloudflight")
+                    url.set("https://cloudflight.io")
+                }
+                developers {
+                    developer {
+                        id.set("klu2")
+                        name.set("Klaus Lehner")
+                        email.set("klaus.lehner@cloudflight.io")
+                    }
+                }
+                scm {
+                    connection.set("scm:ggit@github.com:cloudflightio/spdx-catalog.git")
+                    developerConnection.set("scm:git@github.com:cloudflightio/spdx-catalog.git")
+                    url.set("https://github.com/cloudflightio/spdx-catalog")
+                }
+            }
         }
     }
 }
 
 dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.11.3")
-
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.6.3")
 }
-
