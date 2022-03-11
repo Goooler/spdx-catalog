1f19affcf6c73372e1399b84cf9014248a86688b
Klaus Lehner <172195+klu2@users.noreply.github.com>
2022-03-11 23:10:32 +0100

    support publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,6 +72,16 @@ publishing {
             }
         }
     }
+    repositories {
+        maven {
+            name = "OSSRH"
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
 }
 
 dependencies {
