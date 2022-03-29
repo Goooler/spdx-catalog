b5fe0ccc26e024f76bdf2b6f4ed6d93d488f5c7e
Klaus Lehner <172195+klu2@users.noreply.github.com>
2022-03-29 16:55:08 +0200

    add the possibility to find by url or query

--- src/main/kotlin/io/cloudflight/license/spdx/SpdxLicenses.kt
+++ src/main/kotlin/io/cloudflight/license/spdx/SpdxLicenses.kt
@@ -8,8 +8,10 @@ import java.util.*
 /**
  * Singleton object which parses the SPDX catalog and provides all data in a structured way.
  *
- * @see [SpdxLicenses.getById]
- * @see [SpdxLicenses.getByDescription]
+ * @see [SpdxLicenses.findById]
+ * @see [SpdxLicenses.findByName]
+ * @see [SpdxLicenses.findByUrl]
+ * @see [SpdxLicenses.findLicense]
  *
  * @author Klaus Lehner, Cloudflight
  */
@@ -23,36 +25,60 @@ object SpdxLicenses {
             .use { it.reader().readText() })
 
     private val licensesById = licenseFile.licenses.associateBy { it.licenseId }
-    private val licenseByDescription: Map<String, SpdxLicense>
+    private val licenseByName: Map<String, SpdxLicense>
+    private val licenseByUrl: Map<String, SpdxLicense>
 
     @Serializable
     private class LicenseMappings(
-        @SerialName("idToDescription") val idToDescription: Map<String, List<String>>
+        @SerialName("idToName") val idToName: Map<String, List<String>>,
+        @SerialName("idToUrl") val idToUrl: Map<String, List<String>>
     )
 
+    private fun String.removeProtocol(): String {
+        return removePrefix("https://").removePrefix("http://")
+    }
+
     init {
-        val map = mutableMapOf<String, SpdxLicense>()
+        val mapByName = mutableMapOf<String, SpdxLicense>()
+        val mapByUrl = mutableMapOf<String, SpdxLicense>()
 
         licenseFile.licenses.forEach {
-            if (map.containsKey(it.name.lowercase())) {
+            if (mapByName.containsKey(it.name.lowercase())) {
                 // unfortunately there are some really few entires in licenses.json with duplicate name
                 //throw IllegalArgumentException("Duplicate License name ${it.name}")
             } else {
-                map[it.name.lowercase(Locale.getDefault())] = it
+                mapByName[it.name.lowercase(Locale.getDefault())] = it
+            }
+            if (mapByUrl.containsKey(it.detailsUrl.removeProtocol())) {
+                throw IllegalArgumentException("Duplicate License URL name ${it.name}")
+            } else {
+                mapByUrl[it.detailsUrl.removeProtocol()] = it
             }
         }
-        licenseSynonyms.idToDescription.forEach { entry ->
+        licenseSynonyms.idToName.forEach { entry ->
             val license = licensesById[entry.key]
-                ?: throw IllegalArgumentException("Unknown license ${entry.key} in license-synoyms.json")
+                ?: throw IllegalArgumentException("Unknown license ${entry.key} in license-synonyms.json")
             entry.value.forEach { syn ->
-                if (map.containsKey(syn.lowercase())) {
+                if (mapByName.containsKey(syn.lowercase())) {
                     //throw IllegalArgumentException("Duplicate License name ${syn}")
                 } else {
-                    map[syn.lowercase()] = license
+                    mapByName[syn.lowercase()] = license
                 }
             }
         }
-        licenseByDescription = map.toMap()
+        licenseSynonyms.idToUrl.forEach { entry ->
+            val license = licensesById[entry.key]
+                ?: throw IllegalArgumentException("Unknown license ${entry.key} in license-synonyms.json")
+            entry.value.forEach { syn ->
+                if (mapByUrl.containsKey(syn.removeProtocol())) {
+                    throw IllegalArgumentException("Duplicate License name ${syn}")
+                } else {
+                    mapByUrl[syn.removeProtocol()] = license
+                }
+            }
+        }
+        licenseByName = mapByName.toMap()
+        licenseByUrl = mapByUrl.toMap()
     }
 
     /**
@@ -60,16 +86,61 @@ object SpdxLicenses {
      *
      * @return `null` if the license does not exist
      */
-    fun getById(licenseId: String): SpdxLicense? {
+    fun findById(licenseId: String): SpdxLicense? {
         return licensesById[licenseId]
     }
 
     /**
-     * Access a [SpdxLicense] by the given [description]
+     * Access a [SpdxLicense] by the given [name]
      *
      * @return `null` if the license does not exist
      */
-    fun getByDescription(description: String): SpdxLicense? {
-        return licenseByDescription[description.lowercase()]
+    fun findByName(name: String): SpdxLicense? {
+        return licenseByName[name.lowercase()]
+    }
+
+    /**
+     * Access a [SpdxLicense] by the given [url]
+     *
+     * @return `null` if the license does not exist
+     */
+    fun findByUrl(url: String): SpdxLicense? {
+        return licenseByUrl[url.removeProtocol()]
+    }
+
+    /**
+     * Searches a license given the attached [LicenseQuery] in the following order:
+     * - first we check [LicenseQuery.id]
+     * - then, if ID could not be found, we check [LicenseQuery.url]
+     * - and last, we search for  [LicenseQuery.name]
+     *
+     * @return `null` if no license could be found
+     */
+    fun findLicense(query: LicenseQuery): SpdxLicense? {
+        if (query.id != null) {
+            val l = findById(query.id)
+            if (l != null) {
+                return l
+            }
+        }
+        if (query.url != null) {
+            val l = findByUrl(query.url)
+            if (l != null) {
+                return l
+            }
+        }
+        if (query.name != null) {
+            val l = findByName(query.name)
+            if (l != null) {
+                return l
+            }
+        }
+        return null
     }
 }
+
+data class LicenseQuery(
+    val id: String? = null,
+    val url: String? = null,
+    val name: String? = null
+)
--- src/test/kotlin/io/cloudflight/license/spdx/SpdxLicensesTest.kt
+++ src/test/kotlin/io/cloudflight/license/spdx/SpdxLicensesTest.kt
@@ -7,17 +7,56 @@ class SpdxLicensesTest {
 
     @Test
     fun findLicense() {
-        assertEquals("BSD Zero Clause License", SpdxLicenses.getById("0BSD")?.name)
+        assertEquals("BSD Zero Clause License", SpdxLicenses.findById("0BSD")?.name)
     }
 
     @Test
     fun missingLicense() {
-        assertNull(SpdxLicenses.getById("ANY"))
+        assertNull(SpdxLicenses.findById("ANY"))
     }
 
     @Test
     fun findLicenseByDescription() {
-        val license = SpdxLicenses.getByDescription("The Apache Software License, Version 2.0")
+        val license = SpdxLicenses.findByName("The Apache Software License, Version 2.0")
+        assertNotNull(license)
+        assertEquals("Apache-2.0", license?.licenseId)
+    }
+
+    @Test
+    fun findLicenseByHttpUrl() {
+        val license = SpdxLicenses.findByUrl("http://www.apache.org/licenses/LICENSE-2.0.txt")
+        assertNotNull(license)
+        assertEquals("Apache-2.0", license?.licenseId)
+    }
+
+    @Test
+    fun findLicenseByHttpsUrl() {
+        val license = SpdxLicenses.findByUrl("https://www.apache.org/licenses/LICENSE-2.0.txt")
+        assertNotNull(license)
+        assertEquals("Apache-2.0", license?.licenseId)
+    }
+
+    @Test
+    fun findByQuery_findUrl() {
+        val license = SpdxLicenses.findLicense(
+            LicenseQuery(
+                name = "The Apache Software License",
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            )
+        )
+        assertNotNull(license)
+        assertEquals("Apache-2.0", license?.licenseId)
+    }
+
+    @Test
+    fun findByQuery_findId() {
+        val license = SpdxLicenses.findLicense(
+            LicenseQuery(
+                id = "Apache-2.0",
+                name = "something",
+                url = "something else"
+            )
+        )
         assertNotNull(license)
         assertEquals("Apache-2.0", license?.licenseId)
     }
